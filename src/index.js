d93613c8e4447974d111c535893e5ea02eb57f23
Mahesh Bhalala <maheshbhalala27@gmail.com>
2022-08-04 21:53:34 +0530

    add

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
